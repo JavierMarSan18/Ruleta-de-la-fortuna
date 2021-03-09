4b1c60b7f155994e05449870c0eb5892d50c62ab
Javier <https://github.com/JavierMarSan18>
2021-03-09 13:00:35 -0600

    Agregar opcion para ver premios

--- src/ruleta_de_la_suerte/Ruleta.java
+++ src/ruleta_de_la_suerte/Ruleta.java
@@ -12,12 +12,12 @@ public class Ruleta {
 		boolean condicion = true;
 
 		do {
-
 			System.out.println("||-----------------------||");
 			System.out.println("||         RULETA        ||");
 			System.out.println("||-----------------------||");
 			System.out.println("||1. Jugar a la ruleta   ||");
-			System.out.println("||2. Salir               ||");
+			System.out.println("||2. Ver premios         ||");
+			System.out.println("||3. Salir               ||");
 			System.out.println("||-----------------------||");
 			System.out.print(".: ");
 			op = entry.nextLine();
@@ -25,10 +25,21 @@ public class Ruleta {
 			if (op.equals("1")) {
 				Ruleta(premios);
 			} else if (op.equals("2")) {
+				ImprimirPremios(premios);
+			} else if (op.equals("2")) {
 				condicion = false;
 			}
 		} while (condicion);
 
+	}
+
+	public static void ImprimirPremios(String[] premios) {
+		System.out.println("||-----------------------||");
+		System.out.println("||    Premios Posibles   ||");
+		System.out.println("||-----------------------||");
+		for (int i = 0; i < premios.length; i++) {
+			System.out.println("  -" + premios[i]);
+		}
 	}
 
 	public static void Ruleta(String[] premios) {
